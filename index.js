d7d0c62909dc16117e7d3bb183c99c36184b8d5a
olatunde1998 <65951629+olatunde1998@users.noreply.github.com>
2022-04-19 03:46:55 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+//const hamburger = document.getElementById('hamburger');
+//const navUL = document.getElementById('nav-ul');
+
+//hamburger.addEventListener('click', function(){
+    //navUL.classList.toggle('nav-ul');
+//});
+
+
+//var x = document.getElementsById("collections");
+function clickMe(){
+  //var y = document.getElementById("hamburger");
+  
+   
+
+    
+   if (document.querySelector('li').style.borderBottom === '2px solid red'){
+     document.querySelector('li').style.borderBottom ='none';
+   }
+   else{
+     document.querySelector('li').style.borderBottom = '2px solid red'
+   }
+}
+
+  
